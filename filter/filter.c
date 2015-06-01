e0d1ae8e61443544508b2cc624f414967dc88393
EvgenyZamyatin <jeka_zamyatin@mail.ru>
2015-06-01 11:04:17 +0300

    filter fix

--- filter/filter.c
+++ filter/filter.c
@@ -13,10 +13,8 @@ int main(int argc, char* args[]) {
 	while ((len = read_until(STDIN_FILENO, buf, BUF_LEN, '\n')) > 0) {
 		buf[len-1] = 0;
 		if (spawn(args[1], newargs) == 0) {
-			write_(STDOUT_FILENO, buf, len);
-			buf[0] = '\n';
-			buf[1] = 0;
-			write_(STDOUT_FILENO, buf, 2);
+			write_(STDOUT_FILENO, buf, len-1);
+			write_(STDOUT_FILENO, "\n", 1);
 		}
 	}
 	free(newargs);
